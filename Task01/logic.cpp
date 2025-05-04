eadbe0978a05373334f35c11e3afa07c0f01af97
linchic-l <lebedevskayalina@gmail.com>
2025-05-04 19:25:21 +0300

    Solve task01

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -8,5 +8,22 @@
 #include "logic.h"
 
 double calculate_arithmetical_mean_of_nonzero_elements(int** matrix, int n, int m) {
-	return 0.0;
+	if (matrix == nullptr || n <= 0 || m <= 0) {
+		return 0;
+	}
+	
+	double sum = 0;
+	int count = 0;
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] != 0) {
+				sum += matrix[i][j];
+				count++;
+			}
+		}
+	}
+	return count > 0 ? sum / count : 0;
 }
