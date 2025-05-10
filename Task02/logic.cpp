df5b7f538dc7da046389c8997fc4e6cfac29b8c0
linchic-l <lebedevskayalina@gmail.com>
2025-05-10 17:56:04 +0300

    Solve Task02

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -7,5 +7,24 @@
 #include "logic.h"
 
 int sum_main_and_second_diagonales_elements(int** matrix, int n) {
-	return 0;
+	if (matrix == nullptr || n <= 0) {
+		return 0;
+	}
+	
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += matrix[i][i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += matrix[i][n - 1 - i];
+	}
+	if (n % 2 == 1) {
+		sum -= matrix[n / 2][n / 2];
+	}
+
+	return sum;
 }
